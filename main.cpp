4fedec0acf0cba16c8bf48f70075d673289b3d74
mlanzamolina <39623745+mlanzamolina@users.noreply.github.com>
2019-11-05 14:04:04 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Containt type:text/html \n\n";//converts text output to html
+	cout << "<html>";
+	cout<<"<head><title>Hello web</tittle></head>";
+	cout << "<body>";
+
+	cout << "<h1>Hello web!!!!</H1>";
+	cout<<"<p>Here is your website";
+	cout << "/body></html>";
+	return 0;
+}
