1e99d4af1f83ea68d3e23c0dacd213940406911e
mlanzamolina <39623745+mlanzamolina@users.noreply.github.com>
2019-11-12 11:52:29 -0600

    Update pglist.h

--- pglist.h
+++ pglist.h
@@ -1,44 +1,52 @@
 #pragma once
+
 #ifndef PGLIST_H
 #define PSLIST_H
 #include <string.h>
+#include <fstream>
+#include <direct.h>
+#include <sys/stat.h>
+#include <windows.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <fstream>
+typedef std::vector<std::string> stringvec;
 struct nodoPagina
 {
 	char* nombre;
 	double pageRank;
-	nodoPagina* abajo;
-	nodoPaginaRedirect* siguiente;
-	nodoPagina(char*name):abajo(0),siguiente(0)
+	nodoPagina * abajo;
+	nodoPagina* siguiente;
+	nodoPagina(char*name):abajo(0),siguiente(0),pageRank(0)
 	{
 		nombre = new char[strlen(name)];
 		strcpy(nombre, name);
 	}
 
 };
-struct nodoPaginaRedirect
-{
-	char* nombre;
-	nodoPaginaRedirect* siguiente;
-	nodoPaginaRedirect(char* name) :siguiente(0)
-	{
-		nombre = new char[strlen(name)];
-		strcpy(nombre, name);
-	}
 
-};
 
 class pglist
 {
 private:
 	nodoPagina* primero;
-
+	nodoPagina* primeroBuscar;
 public:
+	pglist():primero(0),primeroBuscar(0)
+	{
+
+	}
+	bool isEmptyB();
 	bool isEmpty();
-	void agregar(nodoPagina**,char*);
-	void agregarLinks();
-	void ordenarCalculo();
-	
-	 
+	void loadList();
+	void read_directory(const std::string& name, stringvec& v);
+	void buscar(std::string keyword_);
+	void print();
+	void printb();
 };
+
 
 #endif // !PGLIST_H
